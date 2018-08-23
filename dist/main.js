d98777b8dcf9e83660e0fc557f30473c8a16e6a1
AGCH <anastasiyache@gmail.com>
2018-08-23 17:15:04 +0300

    render visitors with handlebars

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,25 @@
 var hosts = [];
-var source = $('#hosts-template').html();
+// var source = $('#hosts-template').html();
+var source = $('#visitors-template').html();
 var template = Handlebars.compile(source);
+
+$('.get_visitors').on('click', function () {
+   $.ajax({
+    method: "GET",
+    url: '/get_array',
+    dataType: "json",
+    success: function (data) {
+    console.log("getVISITORS",data);
+    let hosts= {"visitors": data};
+    console.log("HOSTS",hosts);
+    renderVisitors(hosts);
+       },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+})
+
 
 $('.send').on('click', function () {
     let url_host = $('.url').val();
@@ -25,6 +44,12 @@ function AppendData(data_array){
     $('.text').append(newHTML);
 }
 
+function renderVisitors(visitors){
+    console.log("render",visitors);
+    $('.visitors').empty();
+    var newHTML = template(visitors);
+    $('.visitors').append(newHTML);
+}
 //   $('.sendPost').on('click', function () {
 //     let text = $('.message').val();
 //     alert(text);
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const router = express.Router()
-
+var visitors = [];
 router.get('/message/:name', (req, res) => {
     var user_name = req.params.name;
-    var visitors = [];
     let str = 'Hi, ' + user_name +"! How are you? You've visited Anastasia's page.";
     let send_d ={"name": "Nastya", "text": str}
     visitors.push(send_d);
     console.log(send_d);
     res.send(send_d);
-    // res.send("Hello, " + user_name + "!" + " You have just visited my server!<br>" +  
-    // 'You know from what movie these words: "Colour My Life With The Chaos Of Trouble"?')  ;
-    // res.send(JSON.stringify("Hi, you know from what movie these words: 'Colour My Life With The Chaos Of Trouble.'?")) //or whatever message you want
 })
 
+router.get('/get_array', (req, res) => {
+  
+    res.send(visitors);
+})
 router.get('https://hunter.serveo.net/message/Ann', (req, res) => {
     var visitors = [];
     let answer= { "name": res.name, "text": res.text};
